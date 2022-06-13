4712a2a10c6ae54e8aa70326a61b9f7a65154b1c
moonCake <klamina259@gmail.com>
2022-06-13 16:18:35 +0300

    clear

--- app/src/main/java/com/spacemooncake/iteractivetap/domain/entities/Video.kt
+++ app/src/main/java/com/spacemooncake/iteractivetap/domain/entities/Video.kt
@@ -1,5 +1,5 @@
 package com.spacemooncake.iteractivetap.domain.entities
 
 data class Video(val videoResId: Int,
-                 var sourceVideo : String  = "android.resource://com.spacemooncake.interactive/"
+                 var sourceVideo : String  = "android.resource://com.spacemooncake.iteractivetap/"
                          + videoResId)
