12a8358282fa16e647100894a55be4cd325a50d8
moonCake <klamina259@gmail.com>
2022-06-13 14:40:55 +0300

    add all logic

--- app/src/main/java/com/spacemooncake/iteractivetap/ui/video/VideoFragment.kt
+++ app/src/main/java/com/spacemooncake/iteractivetap/ui/video/VideoFragment.kt
@@ -1,5 +1,6 @@
 package com.spacemooncake.iteractivetap.ui.video
 
+
 import android.animation.ObjectAnimator
 import android.graphics.Path
 import android.graphics.RectF
@@ -8,6 +9,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.animation.addListener
 import androidx.fragment.app.Fragment
 import com.spacemooncake.iteractivetap.databinding.VideoFragmentBinding
 import com.spacemooncake.iteractivetap.domain.entities.Video
@@ -20,6 +22,7 @@ class VideoFragment : Fragment() {
     private var _binding: VideoFragmentBinding? = null
     private val binding get() = _binding!!
     private lateinit var sourceVideo: List<Video>
+    private var interactiveIsRunning: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,27 +41,59 @@ class VideoFragment : Fragment() {
 
     private fun bindView() {
         with(binding) {
+            interactiveIsRunning = true
             video.setVideoURI(Uri.parse(sourceVideo[0].sourceVideo))
             video.start()
             butterflyFly()
+            Thread(Runnable {
+                while (interactiveIsRunning) {
+                    val currentDuration = binding.video.duration
+                    val currentPosition = binding.video.currentPosition
+                    val progress = (currentPosition * 100) / currentDuration
+                    binding.timerLeft.progress = progress
+                    binding.timerRight.progress = progress
+                }
+            }).start()
+            butterflyButton.setOnClickListener {
+                interactiveIsRunning = false
+                butterflyButton.visibility = View.GONE
+                video.setVideoURI(Uri.parse(sourceVideo[3].sourceVideo))
+                video.start()
+            }
+
         }
     }
+
 
     private fun butterflyFly() {
         val path1 = Path()
         var rectf = RectF(400F, 100F, 700F, 2000F)
 //      var rectf2 = RectF(400F, 200F, 700F, 1000F)
 //      var rectf3 = RectF(400F, 200F, 1000F, 2000F)
-        path1.addOval(rectf, Path.Direction.CCW )
+        path1.addOval(rectf, Path.Direction.CCW)
 //        path1.addOval(rectf2, Path.Direction.CW )
 //        path1.addOval(rectf3, Path.Direction.CCW )
         var pathAnimator = ObjectAnimator
-            .ofFloat(binding.butterflyButton,
-            "x",
-            "y", path1)
-
-        pathAnimator.duration = 5000L
+            .ofFloat(
+                binding.butterflyButton,
+                "x",
+                "y", path1
+            )
+        pathAnimator.duration = 9000
         pathAnimator.start()
+        pathAnimator.addListener(
+            onEnd = {
+                with(binding) {
+                    if (interactiveIsRunning) {
+                        timerLeft.visibility = View.GONE
+                        timerRight.visibility = View.GONE
+                        butterflyButton.visibility = View.GONE
+                        textTask.visibility = View.GONE
+                        failedTask.visibility = View.VISIBLE
+                    }
+                }
+            }
+        )
 
     }
 
